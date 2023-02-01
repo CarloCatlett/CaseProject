c88f1792e63fa84b7f93955e1391595f5d3e0502
CarloCatlett <122460339+CarloCatlett@users.noreply.github.com>
2023-02-01 00:31:36 -0600

    Update creatureToughness.js

--- TabletopGame/creatureToughness.js
+++ TabletopGame/creatureToughness.js
@@ -1,5 +1,6 @@
-document.getElementById("toughness").onchange=if document.getElementById("toughness").value<=0){
+document.getElementById("toughness").onchange= function(){if document.getElementById("toughness").value<=0){
  document.getElementById("status").placeholder ="dead";
 }else{
  document.getElementById("status").placeholder= "alive";   
 }
+                                                         }
