a01c5cfa3e49af570ab615f5a636324cf342d3c2
CarloCatlett <122460339+CarloCatlett@users.noreply.github.com>
2023-02-07 08:51:43 -0600

    Update mtgAPI.js

--- TabletopGame/cardDatabase/mtgAPI.js
+++ TabletopGame/cardDatabase/mtgAPI.js
@@ -1,5 +1,17 @@
+document.getElementById("search-form").addEventListener("submit", function(event) {
+  event.preventDefault();
+
+  var query = document.getElementById("query").value;
+
+
 setTimeout(fetch('https://api.scryfall.com')
   .then(response => response.json())
   .then(data => {
-    document.getElementById("result").innerHTML = data.result;
+      var results = data.results;
+      var resultList = "";
+      for (var i = 0; i < results.length; i++) {
+        resultList += "<li>" + results[i].title + "</li>";
+      }
+     document.getElementById("results").innerHTML = "<ul>" + resultList + "</ul>";
+    });
   }) 50);
