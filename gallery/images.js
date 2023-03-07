dd68eb4f124bc34269afa670aa5fd80e58c42c9f
CarloCatlett <122460339+CarloCatlett@users.noreply.github.com>
2023-03-07 08:27:16 -0600

    Update images.js

--- gallery/images.js
+++ gallery/images.js
@@ -11,12 +11,12 @@ captions[3]="Placeholder-04";
 captions[4]="Placeholder-05";
 captions[5]="Placeholder-06";
 
-images[0]=img.src='https://picsum.photos/200'
-images[1]=img.src='https://picsum.photos/200'
-images[2]=img.src='https://picsum.photos/200'
-images[3]=img.src='https://picsum.photos/200'
-images[4]=img.src='https://picsum.photos/200'
-images[5]=img.src='https://picsum.photos/200'
+images[0]=img.src='https://picsum.photos/200';
+images[1]=img.src='https://picsum.photos/200';
+images[2]=img.src='https://picsum.photos/200';
+images[3]=img.src='https://picsum.photos/200';
+images[4]=img.src='https://picsum.photos/200';
+images[5]=img.src='https://picsum.photos/200';
 
 var htmlCode = "";
 
