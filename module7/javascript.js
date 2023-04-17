33c4e450fd8128602be9a1fecd15232c2260741e
CarloCatlett <122460339+CarloCatlett@users.noreply.github.com>
2023-04-16 22:44:14 -0500

    Update javascript.js

--- module7/javascript.js
+++ module7/javascript.js
@@ -1,1 +1,19 @@
+var lastVisit = localStorage.getItem("sbloggerVisit");
 
+if (lastVisit === null) {
+  lastVisit = 0;
+}
+
+var links = document.getElementsByTagName("a");
+for (var i = 0; i < links.length; i++) {
+  var href = links[i].getAttribute("href");
+  var date = localStorage.getItem(href);
+
+  if (date !== null && date > lastVisit) {
+    links[i].innerHTML += " (New)";
+  }
+
+  localStorage.setItem(href, Date.now());
+}
+
+localStorage.setItem("sbloggerVisit", Date.now());
